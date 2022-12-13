ad2f60b1ec7acb8cb95667765ebe8ae2e123f55a
Pavani7779 <120479463+Pavani7779@users.noreply.github.com>
2022-12-13 20:12:54 +0530

    enum program

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student X = new student();
+            X.name = " Gold";
+            X.Age = 15;
+            X.Telugu = 65;
+            X.Hindi = 35;
+            X.English = 35;
+            X.Maths = 40;
+            X.Science = 55;
+
+            X.Login();
+            
+
+            student Y = new student();
+            Y.name = "Silver";
+            Y.Age = 25;
+            Y.Telugu = 56;
+            Y.Hindi = 40;
+            Y.English = 36;
+            Y.Maths = 58;
+            Y.Science = 55;
+
+            Y.Login();
+            Console.ReadLine();
+        }
+         
+    }
+}
